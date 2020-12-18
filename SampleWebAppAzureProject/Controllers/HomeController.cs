96ab1f84efb32070b1e985313bb806485d93a8fb
SHRISHTI SONI <SHRISHTI.SONI@lntinfotech.com>
2020-12-18 11:47:13 +0530

    v2

--- SampleWebAppAzureProject/Controllers/HomeController.cs
+++ SampleWebAppAzureProject/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace SampleWebAppAzureProject.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Shri !!!!!";
+            ViewBag.Message = "Shri !!!!! Updated";
 
             return View();
         }
